651342c6cf78d937ef63c68e22be8c656fa16bfe
Matt-Littlefield <matt.littlefield@temple.edu>
2024-07-30 21:39:09 -0400

    added js and html

---
+++ main.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  mainWindow.loadFile('index.html')
+
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+
+// Quits application on close
+app.on('window-all-closed', () => {
+  app.quit()
+})
+
+function getArticleSearch(event) {
+  event.preventDefault()
+  document.getElementById("demo").innerHTML = document.getElementById("searchBar").value
+}
