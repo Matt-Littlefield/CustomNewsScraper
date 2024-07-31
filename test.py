5360eb783bdd6695ff228bcba78b497fa073bf52
Matt-Littlefield <matt.littlefield@temple.edu>
2024-07-31 13:39:50 -0400

    now extracts article text

--- test.py
+++ test.py
@@ -2,23 +2,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import requests
-import time
 
 #driver = webdriver.Chrome()
 
 #driver.quit()
 
-requests = requests.get("https://www.cnn.com/").text
+request = requests.get("https://www.cnn.com/").text
 markup = '<a href="http://example.com/">I linked to <i>example.com</i></a>'
 
 
-soup = BeautifulSoup(requests, features="html.parser")
-
-print(soup.a.prettify())
+soup = BeautifulSoup(request, features="html.parser")
 
 headlines = soup.find_all('a',href=True)
 
 for headline in headlines:
     if headline['href'] and headline['href'][0:5] == '/2024':
-        print(headline['href'])
+        page = "https://www.cnn.com" + headline['href']
+        nextpage = requests.get(page).text
+        next = BeautifulSoup(nextpage,"html.parser")
+        text = next.find_all("p")
+        for p in text:
+            print(p.text)
 
